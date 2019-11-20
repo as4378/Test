8d8f3f0b34258e8c167af0a7f131054f2d956049
Anuraag Srivastava <44287697+as4378@users.noreply.github.com>
2019-11-20 14:03:43 -0700

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,1 +1,35 @@
 
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+add <- function(a, b){
+  a + b
+}
+
+
+addC <- function(a, b){
+
+  result <- .C("addC_interface",
+             a = as.integer(a),
+             b = as.integer(b),
+             sum = integer(1),
+             PACKAGE="NewPackage")
+
+  result$sum
+}
+
+
+
